86603d46af52fc84f853fab222297a4fc370c953
marox19 <nafex100@gmail.com>
2023-08-26 03:11:52 +0300

    Task

---
+++ printf.h
@@ -0,0 +1,21 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+int _printf(const char *format, ...);
+
+#endif /* PRINTF_H */
